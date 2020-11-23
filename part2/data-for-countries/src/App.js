8306c626f7ca1616616de6fffcad62c4332f459d
Onen Allwell Ubi <aleenfestus@gmail.com>
2020-11-23 04:59:44 +0100

    Modify

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newFilter, setNewFilter] = useState("");
+
+  //   useEffect(() => {
+  //     axios.get("https://restcountries.eu/all").then((response) => {
+  //       setPersons(response.data);
+  //     });
+  //   }, []);
+
+  const searchChange = (event) => {
+    setNewFilter(event.target.value);
+  };
+
+  //   const search = countries.filter((country) =>
+  //     country.name.toLowerCase().includes(newFilter.toLowerCase())
+  //   );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        find countries <input onChange={searchChange} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
