f1561c1e212642343f5ba94474092bf2aec6f674
Onen Allwell Ubi <aleenfestus@gmail.com>
2020-11-23 23:06:42 +0100

    Phonebook: Read from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,12 +5,7 @@ import PersonForm from "./components/PersonForm";
 import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
@@ -18,6 +13,7 @@ const App = () => {
   useEffect(() => {
     axios.get("http://localhost:3001/persons").then((response) => {
       setPersons(response.data);
+      setNewName("");
     });
   }, []);
 
